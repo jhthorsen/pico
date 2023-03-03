0df3fa40d99dcdecd00b07bae48419636ed47b5e
Jan Henning Thorsen <jan.henning@thorsen.pm>
2023-03-03 14:58:11 +0900

    Removed remaining @mixin using postcss-pseudo-shorthand

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,13 @@
 import postcssImport from 'postcss-import';
 import postcssNested from 'postcss-nested';
 import postcssPresetEnv from 'postcss-preset-env';
+import postcssPseudoShortHand from 'postcss-pseudo-shorthand';
 
 const config = {
   plugins: [
     postcssImport(),
     postcssNested(),
+    postcssPseudoShortHand(),
     postcssPresetEnv({
       stage: 2,
       features: {
