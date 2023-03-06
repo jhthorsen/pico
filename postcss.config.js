45ab8b44b1eb3968fe0a51d230a92776eb66013f
Jan Henning Thorsen <jan.henning@thorsen.pm>
2023-03-06 13:55:25 +0900

    Fixed colors with variables and alpha
      ...need to turn this into a postcss plugin.

--- postcss.config.js
+++ postcss.config.js
@@ -3,11 +3,48 @@ import postcssNested from 'postcss-nested';
 import postcssPresetEnv from 'postcss-preset-env';
 import postcssPseudoShortHand from 'postcss-pseudo-shorthand';
 
+import Color from 'color';
+
+const processed = Symbol();
+
+const postcssColorVariableShorthand = {
+  postcssPlugin: 'postcss-color-variable-shorthand',
+  Declaration(decl, _params) {
+    if (!decl.prop.endsWith('--hsl')) return;
+    if (decl[processed]) return;
+    decl[processed] = true;
+
+    try {
+      let value = decl.value;
+      const prop = [];
+
+      if (value.match(/^\s*(hsl|rgb)\(.*var\(/)) {
+        value = value.replace(/var\([^)]+\)|\d[^, ]*/, m => {
+          prop.push(m.startsWith('var') ? m : '');
+          return m.startsWith('var') ? '0' : m;
+        });
+      }
+
+      const hsl = Color(value).hsl().array();
+      for (let i = 0; i < hsl.length; i++) {
+        hsl[i] = prop[i] ? prop[i] : hsl[i].toFixed(3);
+      }
+
+      decl.value = `${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%`;
+      decl.after(decl.prop.replace(/--hsl$/, '') + `: hsl(var(${decl.prop}))`);
+    }
+    catch (err) {
+      console.warn(decl.prop + ': ' + err);
+    }
+  },
+};
+
 const config = {
   plugins: [
     postcssImport(),
     postcssNested(),
     postcssPseudoShortHand(),
+    postcssColorVariableShorthand,
     postcssPresetEnv({
       stage: 2,
       features: {
