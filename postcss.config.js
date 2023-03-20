72b929cab0cfb64a193c65cc5282ba4076489432
Jan Henning Thorsen <jan.henning@thorsen.pm>
2023-03-20 19:54:54 +0900

    npm install postcss-color-variable-shorthand

--- postcss.config.js
+++ postcss.config.js
@@ -1,55 +1,14 @@
 import postcssImport from 'postcss-import';
 import postcssNested from 'postcss-nested';
+import postcssColorVariableShorthand from 'postcss-color-variable-shorthand';
 import postcssPseudoShortHand from 'postcss-pseudo-shorthand';
-
-import Color from 'color';
-
-const processed = Symbol();
-
-const postcssColorVariableShorthand = {
-  postcssPlugin: 'postcss-color-variable-shorthand',
-  Declaration(decl, _params) {
-    if (!decl.prop.endsWith('--hsl')) return;
-    if (decl[processed]) return;
-    decl[processed] = true;
-
-    try {
-      let value = decl.value;
-      const prop = [];
-
-      if (value.match(/^\s*(hsl|rgb)\(.*var\(/)) {
-        value = value.replace(/var\([^)]+\)|\d[^, ]*/, m => {
-          prop.push(m.startsWith('var') ? m : '');
-          return m.startsWith('var') ? '0' : m;
-        });
-      }
-
-      const hsl = value === 'hsl(0)' ? [] : Color(value).hsl().array();
-      for (let i = 0; i < hsl.length; i++) {
-        hsl[i] = prop[i] ? prop[i] : hsl[i].toFixed(3);
-      }
-
-      if (hsl.length === 3) {
-        decl.value = `${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%`;
-        decl.after(decl.prop.replace(/--hsl$/, '') + `: hsl(var(${decl.prop}))`);
-      }
-      else if (prop.length == 1) {
-        decl.value = prop[0];
-        decl.after(decl.prop.replace(/--hsl$/, '') + `: hsl(var(${decl.prop}))`);
-      }
-    }
-    catch (err) {
-      console.warn(decl.prop + ': ' + err);
-    }
-  },
-};
 
 const config = {
   plugins: [
     postcssImport(),
     postcssNested(),
     postcssPseudoShortHand(),
-    postcssColorVariableShorthand,
+    postcssColorVariableShorthand(),
   ],
 };
 
